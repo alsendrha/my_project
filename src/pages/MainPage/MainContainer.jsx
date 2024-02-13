b1b9f1d9ac56b43b8eccfbf0b28ece0eb6a5f92f
alsendrha <alsendrha@gmail.com>
2024-02-13 15:40:55 +0900

    mainContainer에 map 사용하여 row표출

--- src/pages/MainPage/MainContainer.jsx
+++ src/pages/MainPage/MainContainer.jsx
@@ -2,14 +2,30 @@ import MainScreen from "../../components/MainScreen";
 import Row from "../../components/Row";
 
 const MainContainer = () => {
-  const city = ["서울", "경기", "인천", "강원"];
+  const city = [
+    "서울",
+    "경기",
+    "인천",
+    "강원",
+    "충북",
+    "충남",
+    "대전",
+    "경북",
+    "경남",
+    "대구",
+    "울산",
+    "부산",
+    "전북",
+    "전남",
+    "광주",
+    "제주",
+  ];
   return (
     <div>
       <MainScreen />
-      <Row city={city[0]} />
-      <Row city={city[1]} />
-      <Row city={city[2]} />
-      <Row city={city[3]} />
+      {city.map((item, index) => {
+        return <Row key={index} city={item} />;
+      })}
     </div>
   );
 };
