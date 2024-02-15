d22923861fb4410235a9f590180be9e82696e44f
alsendrha <alsendrha@gmail.com>
2024-02-15 12:54:22 +0900

    row에 마우스 슬라이드 추가

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -2,6 +2,14 @@ import axios from "../api/api";
 import "./Row.css";
 import DetailModal from "./Modal";
 import { useEffect, useState } from "react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+// import styled from "styled-components";
 
 const Row = ({ city }) => {
   const [dataInfo, setDataInfo] = useState([]);
@@ -35,39 +43,55 @@ const Row = ({ city }) => {
   return (
     <div>
       <h3 className="row_title">{city}</h3>
-      <div className="slider">
-        <div
-          className="slider_arrow_left"
-          onClick={() => {
-            document.getElementById(city).scrollLeft -= window.innerWidth - 80;
-          }}
-        >
-          <span className="arrow">{"<"}</span>
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        breakpoints={{
+          1378: {
+            slidesPerView: 6,
+            slidesPerGroup: 6,
+          },
+          998: {
+            slidesPerView: 5,
+            slidesPerGroup: 5,
+          },
+          625: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+          },
+          0: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+          },
+        }}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
+      >
+        <div className="slider">
+          <div id={city} className="row_posters">
+            {dataInfo.map((item) => (
+              <SwiperSlide key={item.contentid}>
+                <div style={{ display: "inline-block", marginTop: "20px" }}>
+                  <img
+                    className="row_poster"
+                    src={
+                      item.firstimage ? item.firstimage : "images/no_img.jpg"
+                    }
+                    alt="관광지 이미지"
+                    onClick={() => handleClick(item)}
+                  />
+                  <div>
+                    <p className="row_main_title">{item.title}</p>
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </div>
         </div>
-        <div id={city} className="row_posters">
-          {dataInfo.map((item) => (
-            <div key={item.contentid}>
-              <img
-                className="row_poster"
-                src={item.firstimage ? item.firstimage : "images/no_img.jpg"}
-                alt="관광지 이미지"
-                onClick={() => handleClick(item)}
-              />
-              <div>
-                <p className="row_main_title">{item.title}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-        <div
-          className="slider_arrow_right"
-          onClick={() => {
-            document.getElementById(city).scrollLeft += window.innerWidth - 80;
-          }}
-        >
-          <span className="arrow">{">"}</span>
-        </div>
-      </div>
+      </Swiper>
       {modalOpen ? (
         <DetailModal {...selected} setModalOpen={setModalOpen} />
       ) : null}
