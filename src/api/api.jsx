5461c10beefa452e50895d149fa1d2e3542524f7
alsendrha <alsendrha@gmail.com>
2024-02-15 17:32:52 +0900

    deploy

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-  baseURL: "http://apis.data.go.kr/B551011/KorService1/searchKeyword1",
+  baseURL: "https://apis.data.go.kr/B551011/KorService1/searchKeyword1",
   params: {
     serviceKey:
       "D6HvbqfFj6otDTGY3883h0C51xIplWlMUXEF+l5ZX9DTpTTNODdcI/6StO1BbYtjTAtOOKyj25hhnMVj4ASszw==",
